52be3884595c6c198bc8c902542b629161cabd85
Ferla <ferlagamer@hotmail.com>
2025-05-13 09:21:39 -0300

    Remover comentarios

--- Consumer/WineOrderClient/src/main/java/br/com/fiap/winery/ApplicationClient2.java
+++ Consumer/WineOrderClient/src/main/java/br/com/fiap/winery/ApplicationClient2.java
@@ -2,38 +2,29 @@ package br.com.fiap.winery;
 
 import java.net.URI;
 import java.net.URL;
-import javax.xml.namespace.QName; // Ensure this is the correct QName import
+import javax.xml.namespace.QName;
 import jakarta.xml.ws.Service;
 
-// Import the generated JAX-WS classes from the new package
 import br.com.fiap.winery.generated.orderclient.WineStockService;
 import br.com.fiap.winery.generated.orderclient.WineWarningService;
-// If you were to use the generated service implementation classes directly (e.g. new WineStockServiceImplementationService(...))
-// you would import them as well, like:
-// import br.com.fiap.winery.generated.orderclient.WineStockServiceImplementationService;
-// import br.com.fiap.winery.generated.orderclient.WineWarningServiceImplementationService;
 
 
 public class ApplicationClient2 {
     public static void main(String[] args) throws Exception {
-        // Steps 37-40: Setup for WineStockService
         URL urlStock = URI.create("http://localhost:8085/WineStockService?wsdl").toURL();
         QName qNameStock = new QName("http://winery.fiap.com.br/", "WineStockServiceImplementationService");
         Service serviceStock = Service.create(urlStock, qNameStock);
         WineStockService wineStockServicePort = serviceStock.getPort(WineStockService.class);
 
-        // Step 41: Call placeOrder
         String order = wineStockServicePort.placeOrder("Cabernet Sauvignon", 2);
         System.out.println("Order Response (ApplicationClient2):");
         System.out.println(order);
         
-        // Steps 58-61: Setup for WineWarningService
         URL urlWarn = URI.create("http://localhost:8086/WineWarningService?wsdl").toURL();
         QName qNameWarn = new QName("http://winery.fiap.com.br/", "WineWarningServiceImplementationService");
         Service serviceWarn = Service.create(urlWarn, qNameWarn);
         WineWarningService wineWarningServicePort = serviceWarn.getPort(WineWarningService.class);
 
-        // Step 62: Call sendWarn
         String warn = wineWarningServicePort.sendWarn();
         System.out.println("Warning Response (ApplicationClient2):");
         System.out.println(warn);
--- Consumer/WineStockClient/src/main/java/br/com/fiap/winery/ApplicationClient1.java
+++ Consumer/WineStockClient/src/main/java/br/com/fiap/winery/ApplicationClient1.java
@@ -2,31 +2,24 @@ package br.com.fiap.winery;
 
 import java.net.URI;
 import java.net.URL;
-import javax.xml.namespace.QName; // Corrected: JAX-WS often expects this for QName
+import javax.xml.namespace.QName;
 import jakarta.xml.ws.Service;
 
-// Import the generated JAX-WS classes
-import br.com.fiap.winery.generated.WineStockService; // This is the SEI (Service Endpoint Interface)
-// import br.com.fiap.winery.generated.WineStockServiceImplementationService; // This is the generated Service class, not strictly needed if using Service.create
+import br.com.fiap.winery.generated.WineStockService;
+
 
 public class ApplicationClient1 {
     public static void main(String[] args) throws Exception {
-        // Script Step 27: Cria uma URL para o arquivo WSDL
         URL url = URI.create("http://localhost:8085/WineStockService?wsdl").toURL();
         
-        // Script Step 28: Cria um QName com o namespace e nome do serviço
         QName serviceQName = new QName("http://winery.fiap.com.br/", "WineStockServiceImplementationService");
         
-        // Script Step 29: Instancie um objeto "Service", denominado "service", com o método "create" da classe "Service"
         Service service = Service.create(url, serviceQName);
         
-        // Script Step 30: Instancie um objeto "WineStockService", denominado "wineStockService", com o método "getPort"
         WineStockService wineStockServicePort = service.getPort(WineStockService.class);
 
-        // Script Step 31: Chama o método getMenu e armazena o resultado
         String menu = wineStockServicePort.getMenu();
-        
-        // Imprime o menu
+    
         System.out.println("Menu from WineStockService (ApplicationClient1):");
         System.out.println(menu);
     }
